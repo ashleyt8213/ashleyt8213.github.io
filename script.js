71c8502e5010f74bf310fceceb48c5bf1d015027
ashleyt8213 <AShleyt367@nycstudents.net>
2024-09-27 13:24:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Ashley Torres"; // update the part BETWEEN the "quotes"
+let userName = "ashleyt8213"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
